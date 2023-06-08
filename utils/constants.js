e36f6304228301fa7e4c7017cd1e40fd24ab7056
Yashank Tiwari <64902661+yashanktiwari@users.noreply.github.com>
2023-06-09 00:03:08 +0530

    Made some changes

--- utils/constants.js
+++ utils/constants.js
@@ -1,2 +1,2 @@
-module.exports.origin = "https://6481749cf81899293b6d09b0--chimerical-dodol-90039e.netlify.app";
-// export const origin = "http://localhost:3000";
+// module.exports.origin = "https://6481749cf81899293b6d09b0--chimerical-dodol-90039e.netlify.app";
+export const origin = "http://localhost:3000";
